8ee80755b751253fd793da70334bb641bc7d60e3
Flitz <joaovitorbprs@gmail.com>
2022-10-17 19:11:22 -0300

    ainda sem resolvero geojson

--- download.py
+++ download.py
@@ -1,105 +1,107 @@
-#!/usr/bin/python
+# #!/usr/bin/python
 
-import requests
+# Implementar assim que acabar a apresentação da ufba
 
-# overriding requests.Session.rebuild_auth to mantain headers when redirected
+# import requests
 
-class SessionWithHeaderRedirection(requests.Session):
+# # overriding requests.Session.rebuild_auth to mantain headers when redirected
 
-AUTH_HOST = 'urs.earthdata.nasa.gov'
+# class SessionWithHeaderRedirection(requests.Session):
 
-def __init__(self, username, password):
+# AUTH_HOST = 'urs.earthdata.nasa.gov'
 
-    super().__init__()
+# def __init__(self, username, password):
 
-    self.auth = (username, password)
+#     super().__init__()
 
-
-
-# Overrides from the library to keep headers when redirected to or from
-
-# the NASA auth host.
-
-def rebuild_auth(self, prepared_request, response):
-
-    headers = prepared_request.headers
-
-    url = prepared_request.url
+#     self.auth = (username, password)
 
 
 
-    if 'Authorization' in headers:
+# # Overrides from the library to keep headers when redirected to or from
 
-        original_parsed = requests.utils.urlparse(response.request.url)
+# # the NASA auth host.
 
-        redirect_parsed = requests.utils.urlparse(url)
+# def rebuild_auth(self, prepared_request, response):
 
+#     headers = prepared_request.headers
 
-
-        if (original_parsed.hostname != redirect_parsed.hostname) and \
-
-                redirect_parsed.hostname != self.AUTH_HOST and \
-
-                original_parsed.hostname != self.AUTH_HOST:
-
-            del headers['Authorization']
+#     url = prepared_request.url
 
 
 
-    return
+#     if 'Authorization' in headers:
+
+#         original_parsed = requests.utils.urlparse(response.request.url)
+
+#         redirect_parsed = requests.utils.urlparse(url)
 
 
 
-# create session with the user credentials that will be used to authenticate access to the data
+#         if (original_parsed.hostname != redirect_parsed.hostname) and \
 
-username = "USERNAME"
+#                 redirect_parsed.hostname != self.AUTH_HOST and \
 
-password= "PASSWORD"
+#                 original_parsed.hostname != self.AUTH_HOST:
 
-session = SessionWithHeaderRedirection(username, password)
-
-
-
-# the url of the file we wish to retrieve
-
-url = "http://e4ftl01.cr.usgs.gov/MOLA/MYD17A3H.006/2009.01.01/MYD17A3H.A2009001.h12v05.006.2015198130546.hdf.xml"
+#             del headers['Authorization']
 
 
 
-# extract the filename from the url to be used when saving the file
-
-filename = url[url.rfind('/')+1:]   
+#     return
 
 
 
-try:
+# # create session with the user credentials that will be used to authenticate access to the data
 
-# submit the request using the session
+# username = "USERNAME"
 
-response = session.get(url, stream=True)
+# password= "PASSWORD"
 
-print(response.status_code)
-
-
-
-# raise an exception in case of http errors
-
-response.raise_for_status()   
+# session = SessionWithHeaderRedirection(username, password)
 
 
 
-# save the file
+# # the url of the file we wish to retrieve
 
-with open(filename, 'wb') as fd:
-
-    for chunk in response.iter_content(chunk_size=1024*1024):
-
-        fd.write(chunk)
+# url = "http://e4ftl01.cr.usgs.gov/MOLA/MYD17A3H.006/2009.01.01/MYD17A3H.A2009001.h12v05.006.2015198130546.hdf.xml"
 
 
 
-except requests.exceptions.HTTPError as e:
+# # extract the filename from the url to be used when saving the file
 
-# handle any errors here
+# filename = url[url.rfind('/')+1:]   
 
-print(e)
+
+
+# try:
+
+# # submit the request using the session
+
+# response = session.get(url, stream=True)
+
+# print(response.status_code)
+
+
+
+# # raise an exception in case of http errors
+
+# response.raise_for_status()   
+
+
+
+# # save the file
+
+# with open(filename, 'wb') as fd:
+
+#     for chunk in response.iter_content(chunk_size=1024*1024):
+
+#         fd.write(chunk)
+
+
+
+# except requests.exceptions.HTTPError as e:
+
+# # handle any errors here
+
+# print(e)
